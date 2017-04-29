d2df60c77233bf3bcc78217f59a677ecb28e1ab9
Jonathan Bradshaw <lavin@posteo.de>
2017-04-29 14:38:23 -0700

    Add user interactivity: Can click on image to go to next

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const ListItem = (props) => (
-  <div>
-    <h2><pre>{ props.item.town }, {props.item.country}</pre></h2>
-    <h3><pre>Total number of strikes: {props.item.number}</pre></h3>
+   <div>
+    <h2><pre>{ props.item.town }, { props.item.location }, {props.item.country}</pre></h2>
+    <h3><pre>Total number of drone strikes: {props.item.number}</pre></h3>
     <h3><pre>Total number of casualties: {props.item.deaths}</pre></h3>
   </div>
 )
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -10,6 +10,7 @@ class App extends React.Component {
       items: [],
       photo: ""
     }
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -32,10 +33,30 @@ class App extends React.Component {
     });
   }
 
+  handleClick() {
+    $.ajax({
+      url: '/records', 
+      success: function (data) {
+        let record = JSON.parse(data);
+        let apiKey = "&key=AIzaSyAWh923QwLcLQGjH1w4OYOG0_CX8jGHbmE";
+        let photoReference = record[0].photo_reference;
+        let photoQuery = 'https://maps.googleapis.com/maps/api/place/photo?maxheight=500&photoreference=';
+
+        this.setState({
+          items: record,
+          photo: photoQuery + photoReference + apiKey
+        });
+      }.bind(this),
+      error: (err) => {
+        console.log('err', err);
+      }
+    });
+  }
+
   render () {
     return (<div>
-      <h1>Drone Report</h1>
-      <img src={this.state.photo} />
+      <h1><pre>Places</pre></h1>
+      <img src={this.state.photo} onClick={this.handleClick}/>
       <List items={this.state.items}/>
     </div>)
   }
